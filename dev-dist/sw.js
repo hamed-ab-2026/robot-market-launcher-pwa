4b74fb24868d8b3f8a46a4c80a1107eb63aa067c
Hamed <hamedabdollahzadeh.ab@gmail.com>
2026-08-11 12:15:34 +0400

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.91o969c6vq4"
+    "revision": "0.pqcetq5hs48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
